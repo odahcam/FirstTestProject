baf66ae64afdc9561f0a2479b2d7513978d728a2
Luiz Filipe Machado Barni <machado@odahcam.com>
2020-04-29 00:29:05 -0300

    ready camera + ready colliders

---
+++ Assets/Scripts/CustomCarCameraController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCarCameraController : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset;
+    public float followSpeed = 10;
+    public float lookSpeed = 10;
+
+    public void LookAtTarget()
+    {
+        var lookDirection = objectToFollow.position - transform.position;
+        var rot = Quaternion.LookRotation(lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, lookSpeed * Time.fixedDeltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        var targetPos = objectToFollow.position + 
+            objectToFollow.forward * offset.z + 
+            objectToFollow.right * offset.x + 
+            objectToFollow.up * offset.y;
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+    private void Start()
+    {
+        offset = transform.position - objectToFollow.position + offset;
+    }
+}
---
+++ Assets/Scripts/CustomCarController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCarController : MonoBehaviour
+{
+    private float HorizontalInput { get; set; }
+    private float VerticalInput { get; set; }
+    private float SteeringAngle { get; set; }
+
+    public WheelCollider wheelFrontDriver;
+    public WheelCollider wheelFrontPassenger;
+    public WheelCollider wheelRearDriver;
+    public WheelCollider wheelRearPassenger;
+
+    public Transform transformFrontDriver;
+    public Transform transformFrontPassenger;
+    public Transform transformRearDriver;
+    public Transform transformRearPassenger;
+
+    public float maxSteeringAngle = 30;
+    public float motorForce = 50; // torque
+
+
+    public void GetInput() 
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer() 
+    {
+        SteeringAngle = maxSteeringAngle * HorizontalInput;
+        wheelFrontDriver.steerAngle = SteeringAngle;
+        wheelFrontPassenger.steerAngle = SteeringAngle;
+    }
+
+    private void Accelarate()
+    {
+        wheelFrontDriver.motorTorque = VerticalInput * motorForce;
+        wheelFrontPassenger.motorTorque = VerticalInput * motorForce;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(wheelFrontDriver, transformFrontDriver);
+        UpdateWheelPose(wheelFrontPassenger, transformFrontPassenger);
+        UpdateWheelPose(wheelRearDriver, transformRearDriver);
+        UpdateWheelPose(wheelRearPassenger, transformRearPassenger);
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+        var pos = transform.position;
+        var quat = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelarate();
+        UpdateWheelPoses();
+    }
+}
